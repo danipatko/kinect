f7955113d56b50fe1c395880f81a37660c67cda8
danipatko <p8.pada@gmail.com>
2022-12-21 22:18:13 +0100

    bind gestures to virtual controller

---
+++ KinectBinds/KinectBinds/Gestures.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace KinectBinds
+{
+    internal class Gestures
+    {
+        KinectSensor sensor;
+        Skeleton skeleton;
+
+        public enum HandPosition
+        {
+            Unknown = -1,
+            Up = 0,
+            Down = 1,
+            Middle = 2
+        }
+
+        public Gestures(KinectSensor sensor)
+        {
+            // skeleton = new Skeleton();
+            this.sensor = sensor;
+        }
+
+        public void OnFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    var skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                    // tracks the closest one (override using sensor.SkeletonStream.AppChoosesSkeletons = true)
+                    skeleton = skeletons[0];
+                }
+            }
+
+            Console.WriteLine(skeleton.TrackingState);
+
+            CheckRightHandIdle();
+            CheckRightHand();
+            CheckLeftHand();
+            CheckPosition();
+        }
+
+        public (float x, float y) RightHandPosition { get; set; } = (0, 0);
+        private void CheckRightHand()
+        {
+            if (RightHandIdle)
+            {
+                RightHandPosition = (0, 0);
+                return;
+            }
+            Joint hand = skeleton.Joints[JointType.HandRight], thorax = skeleton.Joints[JointType.ShoulderRight];
+            RightHandPosition = (hand.Position.X - thorax.Position.X, hand.Position.Y - thorax.Position.Y);
+        }
+
+        public bool RightHandIdle { get; set; } = true;
+        private const float RightHandIdleOffset = 0.25f;
+        private void CheckRightHandIdle()
+        {
+            Joint hand = skeleton.Joints[JointType.HandRight], thorax = skeleton.Joints[JointType.ShoulderRight];
+            RightHandIdle = hand.TrackingState == JointTrackingState.Tracked && Dist((hand.Position.X, hand.Position.Y), (thorax.Position.X, thorax.Position.Y)) < RightHandIdleOffset;
+        }
+
+        public double LeftHandAngle { get; set; } = double.NaN;
+        private const double LeftHandOffsetDeg = 30f;
+        private HandPosition leftHandPosition = HandPosition.Unknown;
+        private void CheckLeftHand()
+        {
+            Joint hand = skeleton.Joints[JointType.HandLeft], thorax = skeleton.Joints[JointType.ShoulderLeft];
+            if (hand.Position.X > thorax.Position.X)
+            {
+                leftHandPosition = HandPosition.Unknown;
+                LeftHandAngle = double.NaN;
+                return;
+            }
+
+            LeftHandAngle = Math.Atan((hand.Position.Y - thorax.Position.Y) / (thorax.Position.X - hand.Position.X)) * 180 / Math.PI;
+
+            // down
+            if(LeftHandAngle < -LeftHandOffsetDeg && leftHandPosition != HandPosition.Down)
+                LeftHandMoved(new LeftHandEventArgs(leftHandPosition, leftHandPosition = HandPosition.Down));
+            // up
+            else if (LeftHandAngle > LeftHandOffsetDeg && leftHandPosition != HandPosition.Up)
+                LeftHandMoved(new LeftHandEventArgs(leftHandPosition, leftHandPosition = HandPosition.Up));
+            // middle
+            else if(LeftHandAngle < LeftHandOffsetDeg && -LeftHandOffsetDeg < LeftHandAngle && leftHandPosition != HandPosition.Middle)
+                LeftHandMoved(new LeftHandEventArgs(leftHandPosition, leftHandPosition = HandPosition.Middle));
+        }
+
+        public (float x, float y) BodyPosition { get; set; } = (0, 0);
+        public bool BodyCentered { get; set; } = false;
+        private const float BodyCenter = .6f; // the depth where the body is centered
+        private const float BodyOffsetY = .1f;
+        private const float BodyOffsetX = .2f;
+        private void CheckPosition()
+        {
+            float depth = -(skeleton.Position.Z / 3f - BodyCenter);
+            BodyPosition = (skeleton.Position.X, depth);
+            if (-BodyOffsetX < skeleton.Position.X && skeleton.Position.X < BodyOffsetX) BodyPosition = (0, BodyPosition.y); // set x to 0
+            if (-BodyOffsetY < depth && depth < BodyOffsetY) BodyPosition = (BodyPosition.x, 0); // set y to 0
+        }
+
+        private float Dist((float x, float y) a, (float x, float y) b)
+        {
+            return Math.Abs(a.x - b.x) + Math.Abs(a.y - b.y);
+        }
+
+        // event stuff
+
+        public class LeftHandEventArgs : EventArgs
+        {
+            public HandPosition Current { get; }
+            public HandPosition Previous { get; }
+            public LeftHandEventArgs(HandPosition prev, HandPosition curr)
+            {
+                Previous = prev;
+                Current = curr;
+            }
+        }
+
+        public event EventHandler<LeftHandEventArgs> OnLeftMoved;
+        protected virtual void LeftHandMoved(LeftHandEventArgs e)
+        {
+            OnLeftMoved?.Invoke(this, e);
+        }
+    }
+}
--- KinectBinds/KinectBinds/MainWindow.xaml.cs
+++ KinectBinds/KinectBinds/MainWindow.xaml.cs
@@ -1,22 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Kinect;
-// using Nefarius.ViGEm.Client;
-// using Nefarius.ViGEm.Client.Targets;
-// using Nefarius.ViGEm.Client.Targets.Xbox360;
+using Nefarius.ViGEm.Client;
+using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
 
 namespace KinectBinds
 {
@@ -32,36 +21,24 @@ namespace KinectBinds
 
         private KinectSensor sensor;
         private Display display;
+        private Gestures gestures;
 
-        // private ViGEmClient client;
-        // private IXbox360Controller controller;
+        private ViGEmClient client;
+        private IXbox360Controller controller;
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (sensor != null) sensor.Stop();
-            // controller.Disconnect();
+            controller.Disconnect();
         }
-
-        // private void SetCursorPosition(Point point)
-        // {
-        //     Win32.POINT p = new Win32.POINT((int)(point.X * widthRatio), (int)(point.Y * heightRatio));
-        //     if(p.x == 0 && p.y == 0)
-        //     {
-        //         return;
-        //     }
-        //     Console.WriteLine($"x = {p.x}, y = {p.y}");
-        //     Win32.SetCursorPos(p.x, p.y);
-        // }
-
+        
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            Thread.Sleep(5000);
             // create virtual device
-            // client = new ViGEmClient();
-            // controller = client.CreateXbox360Controller();
-            // controller.Connect();
-
-            // controller.SetButtonState(Xbox360Button.A, true);
-            // controller.SetSliderValue(Xbox360Slider.RightTrigger, 255);
+            client = new ViGEmClient();
+            controller = client.CreateXbox360Controller();
+            controller.Connect();
 
             // find a sensor
             foreach (var potentialSensor in KinectSensor.KinectSensors)
@@ -74,14 +51,20 @@ namespace KinectBinds
             }
             if (sensor == null) throw new Exception("Unable to find kinect sensor!");
 
+            gestures = new Gestures(sensor);
+            gestures.OnLeftMoved += HandleLeftMoved;
+
             // start image stream
             display = new Display(sensor);
             DisplayImage.Source = display.ImageSource;
             PositionText.Text = display.skeletonPosition;
     
             sensor.SkeletonStream.Enable();
+            sensor.SkeletonStream.EnableTrackingInNearRange = true;
+
+            sensor.SkeletonFrameReady += gestures.OnFrameReady;
             sensor.SkeletonFrameReady += display.OnFrameReady;
-            display.OnDebugInfo += Log;
+            sensor.SkeletonFrameReady += Log;
 
             try
             {
@@ -93,10 +76,32 @@ namespace KinectBinds
             }
         }
 
-        private void Log(object sender, Display.DebugInfoArgs e)
+        private void HandleLeftMoved(object sender, Gestures.LeftHandEventArgs e)
         {
-            PositionText.Text = e.SkeletonPosition;
+            // shoot
+            if(e.Current == Gestures.HandPosition.Up)
+                controller.SetSliderValue(Xbox360Slider.RightTrigger, byte.MaxValue);
+            // stop crouching
+            else if (e.Current == Gestures.HandPosition.Down)
+                controller.SetButtonState(Xbox360Button.B, false);
+            else if (e.Current == Gestures.HandPosition.Middle)
+            {
+                // stop shooting
+                if (e.Previous == Gestures.HandPosition.Up)
+                    controller.SetSliderValue(Xbox360Slider.RightTrigger, 0);
+                // crouch
+                else
+                    controller.SetButtonState(Xbox360Button.B, true);
+            }
         }
-        
+
+        private void Log(object sender, SkeletonFrameReadyEventArgs _)
+        {
+            PositionText.Text = gestures.BodyPosition.ToString();
+            controller.SetAxisValue(Xbox360Axis.RightThumbX, (short)(gestures.RightHandPosition.x * Int16.MaxValue));
+            controller.SetAxisValue(Xbox360Axis.RightThumbY, (short)(gestures.RightHandPosition.y * Int16.MaxValue));
+            controller.SetAxisValue(Xbox360Axis.LeftThumbX, (short)(gestures.BodyPosition.x * 2 * Int16.MaxValue));
+            controller.SetAxisValue(Xbox360Axis.LeftThumbY, (short)(gestures.BodyPosition.y * 2 * Int16.MaxValue));
+        }
     }
 }
